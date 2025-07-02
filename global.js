7ac495ffd37a4c2df1c99ee552bc85c379bd6657
chechu776 <shameerchechu@gmail.com>
2025-07-02 12:30:38 +0530

    global

---
+++ global.js
@@ -0,0 +1,11 @@
+setTimeout(()=>{
+    console.log("chechu");
+    clearInterval(interval)
+},3000)
+
+const interval = setInterval(()=>{
+    console.log("nadhil");
+},1000)
+
+console.log(__dirname);
+console.log(__filename);
