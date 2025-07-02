8927e21a8c29ef50aee8d7c900249485e1cee1c4
chechu776 <shameerchechu@gmail.com>
2025-07-02 12:45:54 +0530

    require,exports

---
+++ module.js
@@ -0,0 +1,7 @@
+const abc = require("./people")
+
+console.log(abc.people,abc.age);
+
+const os=require("os")
+
+console.log(os.platform(),os.homedir());
---
+++ people.js
@@ -0,0 +1,6 @@
+const people =["nadhil","chechu","sameer"]
+
+const age=[20,21,22]
+
+module.exports={people,age}
+
