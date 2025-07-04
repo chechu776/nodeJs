2a9a38a7767bcbf1605f9b5d27f436599cd15c25
chechu776 <shameerchechu@gmail.com>
2025-07-04 10:12:49 +0530

    save_blogs

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
 const express=require("express");
 const morgan=require("morgan");
+const mongoose=require("mongoose")
+const Blog=require("./models/blogs")
+
 const app=express()
+
+const dbURI="mongodb+srv://chechu:chechu123@chechu.km04jpt.mongodb.net/blogdb"
+mongoose.connect(dbURI)
+.then((result)=>{
+    console.log("db connected")
+    app.listen(3000)
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
 
 app.set('view engine','ejs')
 
 app.use(morgan("dev"))
 
 app.use(express.static('public'))
-app.listen(3000)
+
+
+app.get("/add-blog",(req,res)=>{
+    const blog=new Blog({
+        title:"My Blog",
+        snippet:"about my blog",
+        body:"more about my new blog"
+    })
+
+    blog.save()
+    .then((result)=>{
+        res.send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+        
+    })
+})
+
+app.get("/all-blogs",(req,res)=>{
+    Blog.find()
+    .then((result)=>{
+        res.send(result)
+    })
+    .catch((err)=>{
+        console.log(err);  
+    })
+})
 
 app.get("/",(req,res)=>{
     res.render("index",{title:"Home"})
